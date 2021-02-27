2fac0288d905cf582ddd953a5c38fb9ef084fe0f
marciomarquessouza <marciomultimedia@gmail.com>
2021-02-27 14:30:55 -0300

    feat: add user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,15 @@
+import { Document, Schema, Model, model } from "mongoose";
+
+interface IUser extends Document {
+  email: string;
+  password: string;
+  phone: number;
+}
+
+const UserSchema: Schema = new Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: false },
+});
+
+export const User: Model<IUser> = model("User", UserSchema);
