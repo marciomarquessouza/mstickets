7aef76f6a5dfca3ec507f47d722bd3c5268a0513
marciomarquessouza <marciomultimedia@gmail.com>
2021-02-27 12:53:53 -0300

    chore: add mongodb connection

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,7 +1,22 @@
 import app from "./config/app";
+import mongoose from "mongoose";
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log(`Connected to MongoDB`);
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(`An error happening to connect to MongoDB: ${error}`);
+  }
+};
+
+start();
