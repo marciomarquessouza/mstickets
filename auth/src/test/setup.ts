40eb41c6a66049a780d83c60d401f218f19fd725
marciomarquessouza <marciomultimedia@gmail.com>
2021-02-28 15:29:57 -0300

    test: fixing mongodb and environment configs

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -5,6 +5,7 @@ import app from "../config/app";
 let mongo: any;
 
 beforeAll(async () => {
+  process.env.JWT_KEY = "secret";
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
